62baea4848dc22f64febaf1511f3358d7d595d86
csnand <askmryang@outlook.com>
2018-08-29 08:14:10 +0100

    stage two: proof of work finished

--- Main.go
+++ Main.go
@@ -4,9 +4,80 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"time"
 )
+
+const targetBits = 24
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nounce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PreBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nounce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+
+	nounce := 0
+	maxNounce := math.MaxInt64
+
+	fmt.Printf("Mining the block containing \"%s\" \n", pow.block.Data)
+
+	for nounce < maxNounce {
+		data := pow.prepareData(nounce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		}
+
+		nounce++
+
+	}
+
+	fmt.Println("\n")
+
+	return nounce, hash[:]
+
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nounce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	return hashInt.Cmp(pow.target) == -1
+}
 
 type BlockChain struct {
 	blocks []*Block
@@ -17,18 +88,16 @@ type Block struct {
 	Data         []byte
 	PreBlockHash []byte
 	Hash         []byte
-}
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PreBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-	b.Hash = hash[:]
+	Nounce       int
 }
 
 func NewBlock(data string, preBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), preBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), preBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nounce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nounce = nounce
 	return block
 }
 
@@ -56,7 +125,14 @@ func main() {
 		fmt.Printf("Previous Hash: %x\n", block.PreBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 
+}
+
+func IntToHex(i int64) []byte {
+	return []byte(strconv.FormatInt(i, 16))
 }
